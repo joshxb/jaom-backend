466d4f9e7e7ed578a9931ee824726c13f7d0bb52
joshua.algadipe <joshua.algadipe@trustarc.com>
2023-06-11 22:36:00 +0800

    save

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -22,7 +22,9 @@ class NotificationController extends Controller
     public function currentIndex()
     {
         $user = auth()->user();
-        $notifications = Notification::where('user_id', $user->id)->get();
+        $notifications = Notification::where('user_id', $user->id)
+            ->orderBy('created_at', 'desc')
+            ->get();
 
         if ($notifications->isEmpty()) {
             return response()->json([
@@ -37,22 +39,64 @@ class NotificationController extends Controller
 
     public function store(Request $request)
     {
-        $request->validate([
-            'title' => 'required|string',
-            'notification_object' => 'required|string',
-        ]);
+        $user = auth()->user();
+        if (count($request->userIds) == 0) {
+            $request->validate([
+                'title' => 'required|string',
+                'notification_object' => 'required|string',
+            ]);
 
-        $notification = new Notification();
-        $notification->title = $request->title;
-        $notification->notification_object = $request->notification_object;
-        $notification->user_id = auth()->user()->id;
-        $notification->save();
+            $notification = new Notification();
+            $notification->title = $request->title;
+            $notification->notification_object = $request->notification_object;
+            $notification->user_id = $request->user_id;
+            $notification->save();
 
-        return response()->json([
-            'data' => $notification,
-            'message' => 'Notification created successfully!',
-        ]);
+            return response()->json([
+                'data' => $notification,
+                'message' => 'Notification created successfully!',
+            ]);
+
+        } else {
+            $notifications = collect($request->userIds)->map(function ($user_id) use ($request) {
+                $notification = new Notification();
+
+                if ($request->type == "addInRoomNotification") {
+                    $notification->title = 'Add Member From Room Notification';
+                    $notification->notification_object = json_encode([
+                        'todo_id' => null,
+                        'title' => $request->roomName,
+                        'content' => "You have been added from the group chat by the owner",
+                    ]);
+                } elseif ($request->type == "removeInRoomNotification") {
+                    $notification->title = 'Remove Member From Room Notification';
+                    $notification->notification_object = json_encode([
+                        'todo_id' => null,
+                        'title' => $request->roomName,
+                        'content' => "You have been removed from the group chat by the owner",
+                    ]);
+                } elseif ($request->type == "removeInDeletedRoomNotification") {
+                    $notification->title = 'The Room has Been Deleted Notification';
+                    $notification->notification_object = json_encode([
+                        'todo_id' => null,
+                        'title' => $request->roomName,
+                        'content' => "You have been removed because the room has been deleted by the owner",
+                    ]);
+                }
+
+                $notification->user_id = $user_id;
+                $notification->save();
+
+                return $notification;
+            });
+
+            return response()->json([
+                'data' => $notifications,
+                'message' => 'Notification created successfully!',
+            ]);
+        }
     }
+
 
     public function show($id)
     {
