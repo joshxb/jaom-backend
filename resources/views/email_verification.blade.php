3564c7fd552c91acb96a599b7592fe670bda4564
joshxb <21103808@usc.edu.ph>
2023-07-22 23:05:43 +0800

    add email template

---
+++ resources/views/email_verification.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Email Verification - JAO Ministry</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        .btn-hover-effect:hover {
+            opacity: 0.9;
+            transition: all 0.3s;
+            text-decoration: none;
+        }
+
+        body {
+            font-family: Arial, sans-serif;
+            font-size: 14px;
+        }
+
+        .card-header h4 {
+            font-size: 24px;
+            font-weight: bold;
+        }
+
+        .card-body p {
+            margin-bottom: 1rem;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 offset-md-2">
+                <div class="card mt-5">
+                    <div class="card-header text-white" style="background: rgba(16, 136, 152, 0.866);text-align: center">
+                        <h4>Email Verification - JAO Ministry</h4>
+                    </div>
+                    <div class="card-body">
+                        <p style="text-align: center;">
+                            <img src="path_to_your_image_file.jpg" alt="Hello User Image" style="max-width: 100px;">
+                            <br>
+                            Hello User,
+                        </p>
+                        <p>Welcome to JAO Ministry! We are excited to have you as part of our online group ministry. To create a safe and secure environment for all our members, we kindly request you to complete your registration by verifying your email address. Verifying your email ensures that we can maintain the legibility of accounts and foster a positive experience for everyone involved.</p>
+                        <p>Please take a moment to click the button below to verify your email address:</p>
+                        <p>
+                            <a href="[verification_link]" class="btn text-white btn-hover-effect"
+                                style="background: rgba(16, 136, 152, 0.747">Verify Email Address</a>
+                        </p>
+                        <p>If you did not sign up for an account with JAO Ministry, you can safely ignore this email.
+                        </p>
+                        <p>Thank you,</p>
+                        <p>JAO Ministry</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,4 @@
 <?php
-
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\ConversationController;
-use App\Http\Controllers\FeedbackController;
-use App\Http\Controllers\GroupChatController;
-use App\Http\Controllers\GroupChatImageController;
-use App\Http\Controllers\GroupMessageController;
-use App\Http\Controllers\GroupUserController;
-use App\Http\Controllers\MessageController;
-use App\Http\Controllers\NotificationController;
-use App\Http\Controllers\TodoController;
-use App\Http\Controllers\UpdateController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\UserHistoryController;
-use App\Http\Controllers\UserImagesController;
-use App\Http\Controllers\FAQSController;
-use App\Http\Controllers\DonateTransactionsController;
-use App\Http\Controllers\DonationImageController;
-use App\Http\Controllers\OfferController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -32,8 +12,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('jaom-server', function () {
+Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/email-verification', function () {
+    return view('email_verification');
 });
 
 // Include the API routes
