daec6df04dfbe6640f718b2bbcca9ed539e0e423
joshxb <21103808@usc.edu.ph>
2023-11-05 18:19:22 +0800

    [FIX] - fix issues for sending bible quote email

--- app/Response/Manager/web/BibleGeneratorResponse.php
+++ app/Response/Manager/web/BibleGeneratorResponse.php
@@ -6,7 +6,6 @@ use App\Assets\BibleQuote;
 use App\Mail\BibleQuoteMail;
 use App\Models\User;
 use Carbon\Carbon;
-use Djunehor\Logos\Bible;
 use Exception;
 use Illuminate\Support\Facades\Mail;
 use App\Models\Notification;
@@ -25,60 +24,6 @@ class BibleGeneratorResponse
     public function generateBibleQuote($retryCount = 0)
     {
         try {
-            $bibleBooks = [
-                'Genesis',
-                'Joshua',
-                'Judges',
-                'Ruth',
-                'Ezra',
-                'Nehemiah',
-                'Esther',
-                'Psalms',
-                'Proverbs',
-                'Isaiah',
-                'Jeremiah',
-                'Daniel',
-                'Hosea',
-                'Joel',
-                'Amos',
-                'Obadiah',
-                'Jonah',
-                'Micah',
-                'Nahum',
-                'Zephaniah',
-                'Haggai',
-                'Zechariah',
-                'Malachi',
-                'Matthew',
-                'Mark',
-                'John',
-                'Romans',
-                'Galatians',
-                'Ephesians',
-                'Philippians',
-                'Philemon',
-                'Hebrews',
-                'James',
-                'Jude',
-                'Revelation',
-            ];
-
-            $randomBook = $bibleBooks[array_rand($bibleBooks)];
-            $bible = new Bible();
-
-            $bible->book($randomBook);
-            $bookData = $bible->getBook();
-            $totalChapters = count($bookData['chapters']);
-            $randomChapterNumber = rand(1, $totalChapters);
-
-            $bible->chapter($randomChapterNumber);
-            $chapterData = $bible->getChapter();
-
-            $verses = $chapterData['verses'];
-            $randomVerseNumber = array_rand($verses);
-            $quote = $verses[$randomVerseNumber]['text'];
-            $verse = "{$randomBook} {$randomChapterNumber}:{$randomVerseNumber}";
-
             $users = User::whereNotNull("email_verified_at")->get()->shuffle();
             $currentDay = Carbon::now()->format('l');
 
@@ -94,12 +39,18 @@ class BibleGeneratorResponse
                 'https://github.com/joshxb/joam-project-images/blob/main/JAOM6.jpg?raw=true',
                 'https://github.com/joshxb/joam-project-images/blob/main/JAOM14.jpg?raw=true'
             ];
+
             shuffle($images);
             $randomImages = array_slice(array_unique($images), 0, 3);
 
+            $data = $this->bibleQuotes->getQuote();
+            $randomIndex = array_rand($data);
+            $randomQuote = $data[$randomIndex]['text'];
+            $randomVerse = $data[$randomIndex]['verse'];
+
             $bibleEmailData = [
-                'verse' => $verse,
-                'quote' => $quote,
+                'verse' => $randomVerse,
+                'quote' => $randomQuote,
                 'day' => $currentDay,
                 'randomImages' => $randomImages,
             ];
@@ -114,35 +65,16 @@ class BibleGeneratorResponse
             $notification->notification_object = json_encode([
                 'todo_id' => null,
                 'title' => 'Hello there, bible-quote was successfully sent to respective emails based on scheduled time:',
-                'content' => $verse . ': ' . $quote,
+                'content' => $randomVerse . ': ' . $randomQuote,
             ]);
             $notification->user_id = auth()->user()->id;
             $notification->save();
-
-            $data = $this->bibleQuotes->getQuote();
-
-            $randomIndex = array_rand($data);
-            $randomQuote = $data[$randomIndex]['text'];
-            $randomVerse = $data[$randomIndex]['verse'];
-
-            return [
-                'verse' => ($this->returnZeroOrOne() == 0 || $this->returnZeroOrOne() == 1) ? $randomVerse : $randomVerse,
-                'quote' => ($this->returnZeroOrOne() == 0 || $this->returnZeroOrOne() == 1) ? $randomQuote : $randomQuote,
-                'day' => $currentDay,
-            ];
-
         } catch (Exception $e) {
             if ($retryCount < $this->maxRetries) {
-                // return $this->generateBibleQuote($retryCount + 1);
+                // leave empty code
             } else {
-                return response()->json(['error' => 'An error occurred while generating the Bible quote.'], 500);
+                return response()->json(['error' => 'An error occurred while generating the Bible quote.'], 301);
             }
         }
-    }
-
-    private function returnZeroOrOne()
-    {
-        $randomNumber = rand(0, 2);
-        return $randomNumber;
     }
 }
